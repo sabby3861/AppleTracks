10b22c7f4c6c900c870837feff06b258363ff270
Sanjay Kumar <sabby3861@gmail.com>
2022-01-03 21:53:00 +0000

    Added Cache for image load and Combine for Reactive Programming

---
+++ AppleTracks/AppleTracks/VIPER(TracksView)/Entity/ATImageLoader.swift
@@ -0,0 +1,41 @@
+//
+//  ATImageLoader.swift
+//  AppleTracks
+//
+//  Created by Sanjay Kumar on 03/01/2022.
+//
+
+import Foundation
+import UIKit
+import Combine
+
+final class ATImageLoader {
+    static let shared = ATImageLoader()
+    
+    private let cache: ATImageCacheType
+    private lazy var backgroundQueue: OperationQueue = {
+        let queue = OperationQueue()
+        queue.maxConcurrentOperationCount = 5
+        return queue
+    }()
+    
+    init(cache: ATImageCacheType = ATImageCache()) {
+        self.cache = cache
+    }
+    
+    func loadImage(from url: URL) -> AnyPublisher<UIImage?, Never> {
+        if let image = cache[url] {
+            return Just(image).eraseToAnyPublisher()
+        }
+        return URLSession.shared.dataTaskPublisher(for: url)
+            .map { (data, response) -> UIImage? in return UIImage(data: data) }
+            .catch { error in return Just(nil) }
+            .handleEvents(receiveOutput: {[unowned self] image in
+                guard let image = image else { return }
+                self.cache[url] = image
+            })
+            .subscribe(on: backgroundQueue)
+            .receive(on: RunLoop.main)
+            .eraseToAnyPublisher()
+    }
+}
--- AppleTracks/AppleTracks/VIPER(TracksView)/View/ATTrackTableViewCell.swift
+++ AppleTracks/AppleTracks/VIPER(TracksView)/View/ATTrackTableViewCell.swift
@@ -6,25 +6,38 @@
 //
 
 import UIKit
+import Combine
 
 class ATTrackTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var trackNameLabel: UILabel!
     @IBOutlet weak var artistNameLabel: UILabel!
     @IBOutlet weak var priceLabel: UILabel!
     @IBOutlet weak var artistImageView: UIImageView!
+    private var cancellable: AnyCancellable?
+    private var animator: UIViewPropertyAnimator?
     
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
+    /// Dont forget to stop the animator else the app would crash
+    /// Cancel the AnyCancellable
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        artistImageView.image = nil
+        artistImageView.alpha = 0.0
+        animator?.stopAnimation(true)
+        cancellable?.cancel()
+    }
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
-
+    
     /// Display data on table view cell
     ///
     /// - Parameter data: Results containing all info
@@ -42,6 +55,25 @@ class ATTrackTableViewCell: UITableViewCell {
         
         priceLabel.text = album.price.string
         guard let imageUrl = URL(string: (album.image)!) else { return }
-        artistImageView.load(url: imageUrl)
+        //artistImageView.load(url: imageUrl)
+        cancellable = loadImage(for: imageUrl).sink { [unowned self] image in self.showImage(image: image) }
+    }
+    
+    
+    private func showImage(image: UIImage?) {
+        artistImageView.alpha = 0.0
+        animator?.stopAnimation(false)
+        artistImageView.image = image
+        animator = UIViewPropertyAnimator.runningPropertyAnimator(withDuration: 0.3, delay: 0, options: .curveLinear, animations: {
+            self.artistImageView.alpha = 1.0
+        })
+    }
+    
+    private func loadImage(for url: URL) -> AnyPublisher<UIImage?, Never> {
+        return Just(url)
+            .flatMap({ poster -> AnyPublisher<UIImage?, Never> in
+                return ATImageLoader.shared.loadImage(from: url)
+            })
+            .eraseToAnyPublisher()
     }
 }
